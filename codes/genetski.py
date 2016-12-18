12a7cef7c88de1f539edbad6a226f281ed1e0493
Ivan Lakovic <laky55555@gmail.com>
2016-12-18 22:42:51 +0100

    Implemented crosover

--- codes/genetski.py
+++ codes/genetski.py
@@ -83,37 +83,91 @@ class Genetski():
     def calculate_fittnes_function(self, mini_path):
         a = 2
         b = 2
-        c = 2
-        return (a*self.mini_path_distance(mini_path) + b*self.mini_path_uncleaned_cells(mini_path)
+        c = 4
+        return (-a*self.mini_path_distance(mini_path) + b*self.mini_path_uncleaned_cells(mini_path)
                 + c*self.mini_path_sum_distance(mini_path))
 
 
     #TODO: napraviti da se ne ostaje u istom genu npr (1,1) -> (1,2) -> (2,2) u (1,1) -> (2,2) -> (2,2)
     def find_mutable_genes(self, mini_path):
         saved_neighbours = {}
-        mutable_genes = {}
+        mutable_genes = []
         for (i,j) in mini_path:
             saved_neighbours[(i,j)] = self.get_available_positions(i, j)
 
         for (x,y,z) in zip(mini_path[:-1], mini_path[1:], mini_path[2:]):
             intersection = saved_neighbours[x].intersection(saved_neighbours[z])
             if len(intersection):
-                mutable_genes[y] = intersection
+                #print (intersection, intersection.remove(y))
+                intersection.remove(y)
+                mutable_genes.append(intersection)
 
         if (len(mini_path) >= 2):
-            mutable_genes[mini_path[-2]] = saved_neighbours[mini_path[-2]]
+            mutable_genes.append(saved_neighbours[mini_path[-2]])
+            mutable_genes[-1].remove(mini_path[-1])
         return mutable_genes
 
     def mutation(self, mini_path):
         genes_for_mutation = self.find_mutable_genes(mini_path)
-        chosen_gene = choice(list(genes_for_mutation.keys()))
-        new_gene = sample(genes_for_mutation[chosen_gene], 1)[0]
-        for n, i in enumerate(mini_path):
-            if(i==chosen_gene):
-                mini_path[n] = new_gene
+
+        index = choice(range(1, len(mini_path)))
+        while len(genes_for_mutation[index-1]) < 1:
+            index = choice(range(1, len(mini_path)))
+
+
+        new_gene = sample(genes_for_mutation[index-1], 1)[0]
+
+
+        #TODO: tu treba maknuti mini_path2
+        mini_path2 = mini_path[:]
+        mini_path2[index] = new_gene
+
+        return mini_path2
+
+    def crossover(self, parent1, parent2, point_of_crossing):
+
+        if(parent2[point_of_crossing] in self.neighbours_of(parent1[point_of_crossing-1][0], parent1[point_of_crossing-1][1]) and
+            parent1[point_of_crossing] in self.neighbours_of(parent2[point_of_crossing-1][0], parent2[point_of_crossing-1][1])):
+            child1 = parent1[0:point_of_crossing]
+            child2 = parent2[0:point_of_crossing]
+            child1.extend(parent2[point_of_crossing:len(parent2)])
+            child2.extend(parent1[point_of_crossing:len(parent1)])
+            return child1, child2
+
+        return None
 
 
     def next_move(self):
 
         mini_path = []
         self.generate_mini_path()
+
+
+a = ([['#', '#', '#', '#', '#', '#', '#'],
+      ['#', '.', '.', '.', '.', '.', '#'],
+      ['#', '.', '.', '.', '.', '.', '#'],
+      ['#', '.', '.', '.', '.', '.', '#'],
+      ['#', '.', '.', '.', '.', '.', '#'],
+      ['#', '.', '.', '.', '.', '.', '.', '#']])
+gen = Genetski(a, (2,3))
+#a = gen.generate_mini_paths(3,5,3,3)
+#b = a[1]
+b = [(3, 3), (3, 2), (2, 3), (3, 4), (4, 5), (5, 4)]
+c = [(3, 3), (2, 2), (1, 3), (4, 4), (2, 5), (5, 4)]
+b = [(3, 3), (4 ,3), (5, 3), (5, 4), (5, 5), (5, 6)]
+
+print(gen.crossover(b, c, 1))
+print(gen.crossover(b, c, 2))
+print(gen.crossover(b, c, 3))
+print(gen.crossover(b, c, 4))
+print(gen.crossover(b, c, 5))
+gen.calculate_fittnes_function([(1, 1), (1, 2), (1, 3), (1, 4), (1, 5)])
+gen.calculate_fittnes_function([(1,1), (1, 2), (2, 3)])
+
+"""print(b)
+print(gen.find_mutable_genes(b))
+for i in range(10):
+    mut = gen.mutation(b)
+    print()
+    print(b, mut, gen.calculate_fittnes_function(mut))
+    print(gen.crossover(b, mut, 2))"""
